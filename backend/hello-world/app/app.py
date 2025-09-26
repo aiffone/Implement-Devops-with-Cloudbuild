c28e0e0c2593639d13eb097f952ea1a8e1b26ef8
kayode <aiffone@google.com>
2025-09-26 18:15:49 +0100

    url

--- backend/hello-world/app/app.py
+++ backend/hello-world/app/app.py
@@ -8,6 +8,6 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, World!"
 
-# Run the application
+# Run the application 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)  # Expose the app on port 8080
